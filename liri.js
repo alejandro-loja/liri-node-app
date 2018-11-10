f1fdf6642317cc91a9cc15f60ca2ce195b504cec
Alejandro Loja <alejandro.loja@gmail.com>
2018-11-10 17:52:55 -0500

    initial3

--- liri.js
+++ liri.js
@@ -1,4 +1,12 @@
-var request = require('require.js')
-var moment = require('moment.js');
-var dotenv = require('dotenv.js');
+var request = require('request')
+var moment = require('moment');
+require("./key.js")
 var fs = require('fs');
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var query = process.argv.slice(3).join(" ");
+
+console.log(query);
