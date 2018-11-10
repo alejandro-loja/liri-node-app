5ebace157a9fd858bd133cba33578fc89efe993f
Alejandro Loja <alejandro.loja@gmail.com>
2018-11-10 17:35:07 -0500

    initial2

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var request = require('require.js')
 var moment = require('moment.js');
-var dotenv = require('dotenv.js');
+require('key.js')
 var fs = require('fs');
+
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var query = process.argv[3]
